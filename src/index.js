f50637fe920eff9c73927d001a4e08565e5f752f
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-09-21 10:36:19 +0300

    Add functionality to the list form

--- src/index.js
+++ src/index.js
@@ -10,10 +10,18 @@ app.engine("hbs", handlebars.engine({
 app.set("view engine", "hbs");
 app.set('views', 'src/views');
 
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.post('/', (req, res) => {
+  const newTask = req.body;
+
+  console.log(newTask);
+  
 });
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
